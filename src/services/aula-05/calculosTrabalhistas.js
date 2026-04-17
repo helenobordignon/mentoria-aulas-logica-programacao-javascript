89338d9b89933ff790e7b309f86a28ded8f37f99
Heleno Bordignon <135024897+helenobordignon@users.noreply.github.com>
2026-04-16 22:49:36 -0300

    usando documentação javascript

--- src/services/aula-05/calculosTrabalhistas.js
+++ src/services/aula-05/calculosTrabalhistas.js
@@ -1,8 +1,9 @@
 export function calcularLicenca(sexo) {
     //SE, CASO, QUANDO (Condicionais)
-    if (sexo == 'Masculino') {
+    //.toLowerCase() converte tudo para minúscula antes de comparar
+    if (sexo.toLowerCase() == 'masculino') {
         return 'O funcionário tem direito a 2 semanas de licença paternidade'
-    } else if (sexo == 'Feminino') {
+    } else if (sexo.toLowerCase() == 'feminino') {
         return 'A funcionária tem direito a 12 semanas de licença maternidade'
     } else {
         throw new Error('Sexo não identificado. Por favor, informe se é Masculino ou Feminino.'); // exceção para casos onde o sexo não é identificado
